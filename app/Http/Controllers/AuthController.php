08a8a0b89fb401c8e9700b0f99fdddc4062722a5
Lolu Adelore <adeloreisaac@gmail.com>
2022-02-25 12:54:02 +0100

    Fix

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -29,7 +29,7 @@ class  AuthController extends Controller
 
         $credentials = $request->only('email', 'password');
         if (Auth::attempt($credentials)) {
-            return redirect()->route('/')
+            return redirect()->route('home')
                         ->with('success','Signed in');
         }
 
