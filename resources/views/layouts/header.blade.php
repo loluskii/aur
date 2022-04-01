4de08daaf8566900f9648a0e277a71402ec4a4fc
Lolu Adelore <adeloreisaac@gmail.com>
2022-04-01 16:25:14 +0100

    Countdown

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -15,7 +15,7 @@
     <div class="header mb-3 mb-md-2 mb-lg-2">
         <nav class="navbar navbar-expand-lg navbar-light bg- py-4">
             <div class="container-fluid px-md-5 px-lg-5 px-0">
-                <div class="collapse navbar-collapse" id="">
+                {{-- <div class="collapse navbar-collapse" id="">
                     <ul class="navbar-nav mt-2 mt-lg-0">
                         <li class="nav-item active">
                             <a class="nav-link" href="{{ route('shop') }}">SHOP <span
@@ -24,21 +24,20 @@
                         <li class="nav-item">
                             <a class="nav-link" href="#">SUBSCRIBE TO NEWSLETTER</a>
                         </li>
-                        {{-- <li class="nav-item">
-                        <a class="nav-link" href="#">T-SHIRTS</a>
-                    </li> --}}
                     </ul>
-                </div>
+                </div> --}}
                 <button class="navbar-toggler d-lg-none border-0" type="button" data-bs-toggle="modal"
                     data-bs-target="#mobileSideNav" aria-controls="collapsibleNavId" aria-expanded="false"
                     aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="header-img text-center text-md-center text-lg-start">
+                {{-- <a class="navbar-brand mx-auto" href="/"><img src="{{ secure_asset('images/2611.png') }}"
+                    style="height: 40px" class="img-fluid" alt="" srcset=""></a> --}}
+                {{-- <div class="d-flex justify-content-center">
                     <a class="navbar-brand mx-auto" href="/"><img src="{{ secure_asset('images/2611.png') }}"
                             style="height: 30px" class="img-fluid" alt="" srcset=""></a>
-                </div>
-                <a class="nav-link d-lg-none text-decoration-none text-dark" data-bs-toggle="modal" data-bs-target="#modelId"  href="">CART</a>
+                </div> --}}
+                {{-- <a class="nav-link d-lg-none text-decoration-none text-dark" data-bs-toggle="modal" data-bs-target="#modelId"  href="">CART</a>
                 <div class="collapse navbar-collapse" id="collapsibleNavId">
                     <ul class="navbar-nav ms-auto mt-2 mt-lg-0">
                         <li class="nav-item active">
@@ -51,10 +50,10 @@
                             </a>
                         </li>
                     </ul>
-                </div>
+                </div> --}}
             </div>
         </nav>
-        <ul class="nav justify-content-center py-2 d-none d-md-flex d-lg-flex">
+        {{-- <ul class="nav justify-content-center py-2 d-none d-md-flex d-lg-flex">
             @php
                 $categories = App\Models\Category::all();
             @endphp
@@ -63,7 +62,7 @@
                     <a class="nav-link text-decoration-none text-uppercase text-muted" href="{{ route('product.category', $category->id) }}">{{ $category->name }}</a>
                 </li>
             @endforeach
-        </ul>
+        </ul> --}}
 
     </div>
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 
 @section('styles')
     <link rel="stylesheet" href="{{ secure_asset('video/css/video.css') }}">
-    <style>
+    {{-- <style>
         .subscribe {
             background-image: url("{{ secure_asset('images/backgorund-2.jpg') }}");
             background-position: center;
@@ -20,19 +20,132 @@
             font-weight: bold;
         }
 
-    </style>
+    </style> --}}
+    
+    <style>
+
+
+body{
+    overflow: hidden;
+}
+
+#background-video {
+  width: 100vw;
+  height: 100vh;
+  object-fit: cover;
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  z-index: -1;
+}
+
+
+.content {
+  background: rgba(0, 0, 0, 0.5);
+  color: #f1f1f1;
+  width: 100%;
+  padding-top: 40px;
+}
+
+
+
+
+
+
+
+
+        /* general styling */
+        :root {
+          --smaller: .75;
+        }
+        
+        
+        .container {
+          color: #333;
+          margin: 0 auto;
+          text-align: center;
+        }
+        
+        h1 {
+          font-weight: bold;
+          text-transform: uppercase;
+          font-size: 30px;
+        }
+        
+        li {
+          display: inline-block;
+          font-size: 2.5em;
+          list-style-type: none;
+          padding: 1em;
+          text-transform: uppercase;
+          font-weight: bolder;
+        }
+        
+        li span {
+          display: block;
+          font-size: 6.5rem;
+          font-weight: bolder;
+        }
+        
+        .emoji {
+          display: none;
+          padding: 1rem;
+        }
+        
+        .emoji span {
+          font-size: 4rem;
+          padding: 0 .5rem;
+        }
+        
+        @media all and (max-width: 768px) {
+          h1 {
+            font-size: calc(1.5rem * var(--smaller));
+          }
+          
+          li {
+            font-size: calc(1.125rem * var(--smaller));
+          }
+          
+          li span {
+            font-size: calc(3.375rem * var(--smaller));
+          }
+        }</style>
 @endsection
 
 @section('content')
+<video id="background-video" class="background-tint" loop autoplay="true" muted="muted" webkit-playsinline="true" playsinline="true">
+    <source src="{{ secure_asset('cm-chat-media-video-1:06e9ceca-d11c-55fc-be4f-4c344b1fafb9:1008:0:0.MOV') }}" type="video/mp4">
+    Your browser does not support the video tag.
+</video>
     <div class="container mb-5">
         <div class="iframe-container placeholder-glow" id="atlanticlight">
-            <video width="1220" height="500" loop autoplay="true" muted="muted" webkit-playsinline="true"
+            <div class="container content" style="margin-top: 20%">
+            
+                <h1 class="text-white" id="headline">Countdown to launch</h1>
+                <div id="countdown">
+                  <ul>
+                    <li class="mb-5 text-white"><span class="mb-5 text-white" id="days"></span>days</li>
+                    <li class="mb-5 text-white"><span class="mb-5 text-white" id="hours"></span>Hours</li>
+                    <li class="mb-5 text-white"><span class="mb-5 text-white" id="minutes"></span>Minutes</li>
+                    <li class="mb-5 text-white"><span class="mb-5 text-white" id="seconds"></span>Seconds</li>
+                  </ul>
+                </div>
+                <div id="content" class="emoji">
+                  <span>🥳</span>
+                  <span>🎉</span>
+                  <span>🎂</span>
+                </div>
+              </div>
+            {{-- <video width="1220" height="500" loop autoplay="true" muted="muted" webkit-playsinline="true"
                 playsinline="true">
                 <source
                     src="{{ secure_asset('cm-chat-media-video-1:06e9ceca-d11c-55fc-be4f-4c344b1fafb9:1008:0:0.MOV') }}"
                     type="video/mp4">
                 Your browser does not support the video tag.
-            </video>
+            </video> --}}
+            
             {{-- <button>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" id="playpause"
                 xmlns:xlink="http://www.w3.org/1999/xlink">
@@ -78,7 +191,7 @@
         </script> --}}
         </div>
     </div>
-    <div class="container">
+    {{-- <div class="container">
         <div class="row g-4">
             @foreach ($products as $product)
                 <div class="col-md-4 col-12 mb-5 pe-2">
@@ -125,5 +238,53 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> --}}
+@endsection
+
+@section('scripts')
+    <script>
+        (function () {
+  const second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24;
+
+  //I'm adding this section so I don't have to keep updating this pen every year :-)
+  //remove this if you don't need it
+  let today = new Date(),
+      dd = String(today.getDate()).padStart(2, "0"),
+      mm = String(today.getMonth() + 1).padStart(2, "0"),
+      yyyy = today.getFullYear(),
+      nextYear = yyyy + 1,
+      dayMonth = "05/01/",
+      birthday = dayMonth + yyyy;
+  
+  today = mm + "/" + dd + "/" + yyyy;
+  if (today > birthday) {
+    birthday = dayMonth + nextYear;
+  }
+  //end
+  
+  const countDown = new Date(birthday).getTime(),
+      x = setInterval(function() {    
+
+        const now = new Date().getTime(),
+              distance = countDown - now;
+
+        document.getElementById("days").innerText = Math.floor(distance / (day)),
+          document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+          document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+          document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+
+        //do something later when date is reached
+        if (distance < 0) {
+          document.getElementById("headline").innerText = "It's my birthday!";
+          document.getElementById("countdown").style.display = "none";
+          document.getElementById("content").style.display = "block";
+          clearInterval(x);
+        }
+        //seconds
+      }, 0)
+  }());
+    </script>
 @endsection
